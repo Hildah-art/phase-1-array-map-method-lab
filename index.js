18292f23565b7415d2571c21b06db549792b8955
hildahnjaga <hildah@DESKTOP-JDVODGD.>
2025-03-11 22:49:12 +0300

    using the array map method

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
-const titleCased = () => {
-  return tutorials
-}
+    'what does the this keyword mean?',
+    'What is the Constructor OO pattern?',
+    'implementing Blockchain Web API',
+    'The Test Driven Development Workflow',
+    'What is NaN and how Can we Check for it',
+    'What is the difference between stopPropagation and preventDefault?',
+    'Immutable State and Pure Functions',
+    'what is the difference between == and ===?',
+    'what is the difference between event capturing and bubbling?',
+    'what is JSONP?'
+  ];
+  
+  function titleCased() {
+    const titleCased2 = tutorials.map(sentence => {
+      return sentence.split(' ') //split converts  a string to array of substrings based on specified operator(hence i used a space )
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1)) //capitalizing the first letter of each word
+        .join(' '); //join is an array method that joins an array of strings to a single string using a specified operator as well(the space)
+    });
+  
+    return titleCased2; // Return the modified array new(since we are using .map)
+  }
+  
+  console.log(titleCased());
